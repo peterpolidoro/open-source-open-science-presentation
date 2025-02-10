9d41f9988ddaced49f02506f7ec6354e401bd27c
Peter Polidoro <peter@polidoro.io>
2025-02-10 10:35:33 -0500

    Add dark-mode css

--- build-site.el
+++ build-site.el
@@ -20,7 +20,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"css/simple.css\" />")
+      org-html-head "<link rel=\"stylesheet\" href=\"css/simple.css\" /> <link rel=\"stylesheet\" href=\"css/dark-mode.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
