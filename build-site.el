9708db27c88e2e0cc4c00e2346225bd134a06c90
Peter Polidoro <peter@polidoro.io>
2025-02-10 10:57:46 -0500

    Test terminal.css

--- build-site.el
+++ build-site.el
@@ -20,7 +20,8 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"css/simple.css\" /> <link rel=\"stylesheet\" href=\"css/dark-mode.css\" />")
+      ;; org-html-head "<link rel=\"stylesheet\" href=\"css/simple.css\" /> <link rel=\"stylesheet\" href=\"css/dark-mode.css\" />"
+      org-html-head "<link rel=\"stylesheet\" href=\"css/terminal.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
